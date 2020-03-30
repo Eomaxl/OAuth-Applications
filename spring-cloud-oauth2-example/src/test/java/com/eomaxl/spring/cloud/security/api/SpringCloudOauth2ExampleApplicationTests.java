8691bf6d6106a3230ef84b52ce1ab0cc8731b7f8
souravmansingh <souravmansingh@192.168.0.102>
2020-03-30 09:22:14 +0530

    Initial commit

---
+++ spring-cloud-oauth2-example/src/test/java/com/eomaxl/spring/cloud/security/api/SpringCloudOauth2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eomaxl.spring.cloud.security.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOauth2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
